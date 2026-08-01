4f7c4c5ac163c822443a6543408f5d193175d33e
vattmplp-cyber <vattmplp@gmail.com>
2026-08-01 11:19:47 -0500

    Update module.c

--- kernel/module.c
+++ kernel/module.c
@@ -268,7 +268,7 @@ static void module_assert_mutex_or_preempt(void)
 #endif
 }
 
-static bool sig_enforce = false(CONFIG_MODULE_SIG_FORCE);
+static bool sig_enforce = IS_ENABLED(CONFIG_MODULE_SIG_FORCE);
 module_param(sig_enforce, bool_enable_only, 0644);
 
 /*
